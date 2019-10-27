e50c98ae4b9798ba6fd7977833820d2384c1777a
James Mills <james@jamesmills.co.uk>
2019-10-27 15:50:32 +0400

    Removing my API keys. What a classic mistake (yes I have removed these from DataDog too)

--- src/config/datadog.php
+++ src/config/datadog.php
@@ -10,7 +10,6 @@ return [
     |
     */
 
-    'host' => 'https://api.datadoghq.com/api/v1/',
-    'api_key' => '043020c41cf3b64af162b660a5bb7160',
-    'application_key' => '8498f3ec13f79ce0d57ee68802cdd80ee212fc6d',
+    'host' => env('DATADOG_HOST', 'https://api.datadoghq.com/api/v1/'),
+    'api_key' => env('DATADOG_KEY', null),
 ];
